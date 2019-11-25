d449528be1762af9a7e15ecfb5ac1b06e243a4dc
Simon Fischer <kleba@posteo.de>
2019-11-25 10:54:24 +0100

    Correction in RunSetup.ps1

--- Scripts/RunSetup.ps1
+++ Scripts/RunSetup.ps1
@@ -55,7 +55,7 @@ if (-not($connected)) {
     }
     catch {
         Write-Host "Please login first to select the correct subscription"
-        Connect-AzAccount
+        Connect-AzAccount -Force | Out-Null
         $context = Get-AzSubscription -SubscriptionName $subscriptionName
         Set-AzContext $context | Out-Null
     }
